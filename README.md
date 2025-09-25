764f8dacebf61b390dd294418c8c02df7b4e00c0
Víctor Pagola <victorpagola.w@gmail.com>
2025-09-25 18:25:18 +0200

    upd: README.md and App.java, proyecto finished

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## FACTURA
 
 Hacer una factura con los diferentes tipos de print que hay y los sistemas de %
+
+Resultado:
+![image](<Captura de pantalla 2025-09-25 181909.png>)
--- src/App.java
+++ src/App.java
@@ -1,10 +1,13 @@
 public class App {
+
     public static void main(String[] args) throws Exception {
 
-    
-
     System.out.println("Rojo Polo Paella Inc.\nCarretera Muelle 38\n37531 Ávila, Ávila\n");
-    System.out.printf(ut.BLUE +"╔%-20s╦ %-20s╦ %-20s╦ %20s ╗ %n" + ut.RESET, "FACTURAR A", "ENVIAR A", "Nº DE FACTURA", "ES-001");
+    System.out.printf("╔%s%-20s%s╦ %s%-20s%s╦ %s%-20s%s╦%s%20s%s  ╗ %n",
+      ut.BLUE, "FACTURAR A",   ut.RESET,
+    ut.BLUE, "ENVIAR A",     ut.RESET,
+    ut.BLUE, "Nº DE FACTURA",ut.RESET,
+     ut.BLUE, "ES-001", ut.RESET);
     System.out.println("╠════════════════════╬═════════════════════╬═════════════════════╬══════════════════════╣");
     System.out.printf("║%-20s║ %-20s║ %-20s║ %20s ║ %n", "Leda Villaneal", "Leda Villareal", "FECHA", "29.01.2019");
     System.out.println("╠════════════════════╬═════════════════════╬═════════════════════╬══════════════════════╣");
@@ -15,17 +18,23 @@ public class App {
     System.out.printf("%-20s  %-20s ║ %-20s║ %20s ║ %n", "", "", "VENCIMIENTO", "29.01.2019");
     System.out.printf("%89s%n%n", "╚═════════════════════╩══════════════════════╝");
     System.out.println("───────────────────────────────────────────────────────────────────────────────────────");
-    System.out.printf(ut.BG_CYAN + "%-43s %43s %n", "TOTAL FACTURA", 199.65+"$" + ut.RESET);
+    System.out.printf(ut.BOLD + "%-43s %43s %n" + ut.RESET, "T O T A L  F A C T U R A", "1 9 9 . 6 5 $");
     System.out.println("───────────────────────────────────────────────────────────────────────────────────────\n");
-    System.out.printf(ut.BG_RED + "%-10s▒ %-40s▒ %20s▒ %10s ▒" + ut.RESET + "%n", "CANT.", "DESCRIPCIÓN", "PRECIO UNITARIO ", "IMPORTE");
-    System.out.println("═══════════════════════════════════════════════════════════════════════════════════════");
-    System.out.printf("%-10s║ %-40s║ %20s║ %10s ║ %n", "1", "Talla pequeña traje de luces en rojo", "100.00 ", "100.00");
-    System.out.println("───────────────────────────────────────────────────────────────────────────────────────");
-    System.out.printf("%-10s║ %-40s║ %20s║ %10s ║ %n", "2", "Mui grande churrolito", "25.00 ", "50.00");
-    System.out.println("───────────────────────────────────────────────────────────────────────────────────────");
-    System.out.printf("%-10s║ %-40s║ %20s║ %10s ║ %n", "3", "Equipaje de futbol", "5.00 ", "15.00");
-    System.out.println("───────────────────────────────────────────────────────────────────────────────────────");
-    System.out.printf("%-10s  %-40s║ %20s║ %10s ║ %n %n", "", "", "Subtotal ", "165.00");
-    System.out.printf("%-10s  %-40s║ %20s║ %10s ║ %n %n", "", "", "IVA 21.0% ", "34,65");
+    System.out.printf("╔%s %-8s%s╦%s %-40s%s╦%s %20s%s╦%s %10s %s╗%n",
+     ut.BG_GREEN, "CANT.", ut.RESET,
+      ut.BG_GREEN, "DESCRIPCIÓN",ut.RESET,
+      ut.BG_GREEN,"PRECIO UNITARIO ",ut.RESET,
+      ut.BG_GREEN,  "IMPORTE" ,ut.RESET);
+    System.out.println("╠═════════╬═════════════════════════════════════════╬═════════════════════╬════════════╣");
+    System.out.printf("║ %-8s║ %-40s║ %20s║ %10s ║ %n", "1", "Talla pequeña traje de luces en rojo", "100.00 ", "100.00");
+    System.out.println("╠═════════╬═════════════════════════════════════════╬═════════════════════╬════════════╣");
+    System.out.printf("║ %-8s║ %-40s║ %20s║ %10s ║ %n", "2", "Mui grande churrolito", "25.00 ", "50.00");
+    System.out.println("╠═════════╬═════════════════════════════════════════╬═════════════════════╬════════════╣");
+    System.out.printf("║ %-8s║ %-40s║ %20s║ %10s ║ %n", "3", "Equipaje de futbol", "5.00 ", "15.00");
+    System.out.println("╚═════════╩═════════════════════════════════════════╬═════════════════════╬════════════╣");
+    System.out.printf("%-10s  %-40s║ %20s║ %10s ║ %n", "", "", "Subtotal ", "165.00");
+    System.out.printf("%88s%n", "╠═════════════════════╬════════════╣");
+    System.out.printf("%-10s  %-40s║ %20s║ %10s ║ %n", "", "", "IVA 21.0% ", "34,65");
+    System.out.printf("%88s%n%n", "╚═════════════════════╩════════════╝");
 }   
 }
